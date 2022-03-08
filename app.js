61a5ae12708c6a80cae2b1d5b304b7aec51de5e7
William <willyknapp@gmail.com>
2022-03-08 11:26:59 -0500

    enable cors using cors npm module

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const db = require("./controllers/messageController.js");
+
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
